dc72d6157b61f8bfe625f8c835eb85059abdd9ba
Nicollas Isabel <nicollasisabel@hotmail.com>
2018-07-18 21:14:26 -0300

    Finished implementing carrefour crawler and created products table

--- app/business/crawlers/carrefour_crawler.rb
+++ app/business/crawlers/carrefour_crawler.rb
@@ -8,7 +8,7 @@ module Crawlers
 
     class << self
       def execute
-        products = []
+        @products = []
         home = Nokogiri::HTML(open(CARREFOUR_HOME_URL))
 
         # Get all catergories links
@@ -17,18 +17,43 @@ module Crawlers
         # Visit all caterories
         links.each_with_index do |link, index|
           begin
+            puts '+++++++++++++++++++++++++++++++++++++++ PROXIMA CATEGORIA +++++++++++++++++++++++++++++++++++++++'
             category = Nokogiri::HTML(open("#{CARREFOUR_BASE_URL}#{link}"))
 
-            category.css('.prd-info').each do |product|
-              products << [product.css('.prd-name').text().strip,
-                            product.css('.prd-price-new').text().gsub('R$', '').gsub(',', '.').strip.to_f]
+            # Loop the first products
+            loop_through_category(category)
+
+            # Click next button and continue visiting products
+            next_page = (category.css('#loadNextPage').attr('href').value rescue nil)
+
+            # Stop only of next page is nil
+            until next_page.nil?
+              puts '---------------------------------------- PROXIMA PÁGINA ----------------------------------------'
+              category = Nokogiri::HTML(open(next_page))
+
+              loop_through_category(category)
+
+              next_page = (category.css('#loadNextPage').attr('href').value rescue nil)
             end
           rescue
             puts "Erro! Vida que segue!"
           end
         end
 
-        products
+        Product.create!(@products)
+      end
+
+      private
+
+      def loop_through_category(category)
+        category.css('.prd-info').each do |product|
+          puts "#{product.css('.prd-name').text().strip}: #{product.css('.prd-price-new').text().gsub('R$', '').gsub(',', '.').strip.to_f}"
+          @products << { name: product.css('.prd-name').text().strip,
+                         price: product.css('.prd-price-new').text().gsub('R$', '').gsub(',', '.').strip.to_f,
+                         image: '',
+                         market: 'carrefour'
+                       }
+        end
       end
     end
   end
