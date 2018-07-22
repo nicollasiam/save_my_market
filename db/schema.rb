0d8e40e9a047edcc7bf3ab230f91aa6fa38fbd90
Nicollas Isabel <nicollasisabel@hotmail.com>
2018-07-21 23:32:25 -0300

    Created Market controller and model. Started implementing index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_225414) do
+ActiveRecord::Schema.define(version: 2018_07_22_012207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "markets", force: :cascade do |t|
+    t.string "name"
+    t.string "logo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
