0061cfba1953d9607001bbf8731cacc75084395b
Nicollas Isabel <nicollasisabel@hotmail.com>
2018-07-22 20:37:29 -0300

    Fixed markets routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'markets#index'
 
-  resources :market, only: %i(index, show)
+  resources :markets, only: %i(index, show)
   resources :products, only: :show
 end
