ceb1760e9aaf137360554c0c10f75521580dd64c
Nicollas Isabel <nicollasisabel@hotmail.com>
2018-07-29 16:48:15 -0300

    3-colum table

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  paginates_per 20
+  paginates_per 18
 
   belongs_to :market, inverse_of: :products
   belongs_to :category, inverse_of: :products, optional: true
