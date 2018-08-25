78b6702676174dbbaa2ebc8cae13f091951eb520
Nicollas Isabel <nicollasisabel@hotmail.com>
2018-08-25 18:24:28 -0300

    Improved Carrefour crawlers

---
+++ app/business/crawlers/carrefour/registration_crawler.rb
@@ -0,0 +1,83 @@
+module Crawlers
+  module Carrefour
+    class RegistrationCrawler < Crawlers::ApplicationCrawler
+      CARREFOUR_BASE_URL = 'https://www.carrefour.com.br'.freeze
+      CARREFOUR_HOME_URL = 'https://www.carrefour.com.br/dicas/mercado'.freeze
+
+      CARREFOUR_MODEL = Market.find_by(name: 'Carrefour')
+      CARREFOUR_PRODUCTS = CARREFOUR_MODEL.products.pluck(:name)
+
+      require 'nokogiri'
+      require 'open-uri'
+
+      class << self
+        def execute
+          @products = []
+          home = Nokogiri::HTML(open(CARREFOUR_HOME_URL))
+
+          # Get all catergories links
+          links = home.css('#boxes-menulateral ul li ul li a').map { |category| category.attr('href') }
+
+          # Visit all caterories
+          links.each do |link|
+            begin
+              # puts '+++++++++++++++++++++++++++++++++++++++ PROXIMA CATEGORIA +++++++++++++++++++++++++++++++++++++++'
+              category = Nokogiri::HTML(open("#{CARREFOUR_BASE_URL}#{link}"))
+              # Loop the first products
+              loop_through_category(category)
+
+              # Click next button and continue visiting products
+              next_page = (category.css('#loadNextPage').attr('href').value rescue nil)
+
+              # Stop only of next page is nil
+              until next_page.nil?
+                # puts '---------------------------------------- PROXIMA PÁGINA ----------------------------------------'
+                category = Nokogiri::HTML(open(next_page))
+
+                loop_through_category(category)
+
+                next_page = (category.css('#loadNextPage').attr('href').value rescue nil)
+              end
+            rescue
+              puts "Erro! Vida que segue!"
+            end
+          end
+        end
+
+        private
+
+        def loop_through_category(category)
+          category.css('[itemprop=itemListElement]').each_with_index do |product_html, index|
+            product_name = product_html.css('[itemprop=name]')[index].attr('content').strip
+            price = product_html.css('[name=productPostPrice]')[index].attr('value').strip.to_f
+            product_url = product_html.css('[itemprop=url]')[index].attr('content').strip
+
+            next if include_wrong_encoding_chars?(product_name)
+
+            # Fix product name if it has wrong encoding
+            # So it is not added again in the database
+            product_name = Applications::NurseBot.treat_product_name(product_name) if is_sick?(product_name)
+            # Product already exists in database
+
+            unless CARREFOUR_PRODUCTS.include?(product_name)
+              # Add it to the database
+              product = Product.create(name: product_name,
+                                        price: price,
+                                        image: (product_html.css('[itemprop=image]')[index].attr('data-src').strip rescue ''),
+                                        market_name: 'carrefour',
+                                        market: CARREFOUR_MODEL,
+                                        url: product_url)
+
+              # create the first price history
+              new_price = PriceHistory.create(old_price: 0,
+                                              current_price: price,
+                                              product: product)
+
+              puts "NOVO PRODUTO: #{product.name} -> #{product.price} "
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ app/business/crawlers/carrefour/update_crawler.rb
@@ -0,0 +1,49 @@
+module Crawlers
+  module Carrefour
+    class UpdateCrawler < Crawlers::ApplicationCrawler
+      CARREFOUR_PRODUCTS = Product.where(market_name: 'carrefour')
+
+      class << self
+        def execute
+          update_carrefour_products
+        end
+
+        private
+
+        def update_carrefour_products
+          CARREFOUR_PRODUCTS.each do |product_model|
+            begin
+              product_html = Nokogiri::HTML(open(product_model.url))
+
+              price = product_html.css('.priceBig').text.split("\n").last
+                                  .strip.gsub('R$', '').gsub(',', '.')
+                                  .strip.to_f
+
+              # check if price changed
+              # do nothing if it did not
+              if product_model.price != price
+                # if it changed, create a new price history and add it to the product
+                new_price = PriceHistory.create(old_price: product_model.price,
+                                                current_price: price,
+                                                product: product_model)
+
+                product_model.update(price: price)
+                puts "PRODUTO ATUALIZADO. #{product_model.name}: #{product_model.price_histories.order(:created_at).last.old_price} -> #{product_model.price_histories.order(:created_at).last.current_price}"
+              end
+            rescue
+              # TODO: Implement availability attribute
+              # check if product is available
+              if product_html.css('.text-not-product-avisme').text.present?
+                # product_model.update(available: false)
+                puts "PRODUTO INDISPONÍVEL: #{product_model.name} (#{product_model.market.name})"
+              else
+                puts "URL ZUADA: #{product_model.name} (#{product_model.market.name})"
+              end
+
+            end
+          end
+        end
+      end
+    end
+  end
+end
